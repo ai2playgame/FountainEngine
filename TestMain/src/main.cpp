d91aef0c2aa06835e551757e9e081048a90fa2d5
ai2playgame <detective9684@gmail.com>
2021-02-07 02:04:27 +0900

    Initial commit

---
+++ TestMain/src/main.cpp
@@ -0,0 +1,25 @@
+﻿#include <Windows.h>
+#ifdef _DEBUG
+#include <iostream>
+#endif
+
+// あいうえお
+void DebugOutputFormatString(const char* format, ...)
+{
+#ifdef _DEBUG
+	va_list valist;
+	va_start(valist, format);
+	printf(format, valist);
+	va_end(valist);
+#endif
+}
+
+#ifdef _DEBUG
+int main() {
+#else
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+#endif
+	DebugOutputFormatString("Show window test");
+	return 0;
+}
+
