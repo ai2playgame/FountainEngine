455c59c3d08b2a4b04c480af2e4a17ae709e85fb
ai2playgame <detective9684@gmail.com>
2021-02-21 00:22:43 +0900

    Logger追加

---
+++ EngineCore/src/Core/Utility/Logger/Logger.h
@@ -0,0 +1,55 @@
+﻿#pragma once
+#include <string>
+#include <fmt/core.h>
+
+namespace fne {
+
+struct LogReportData {
+	std::string fileName;
+	std::string funcName;
+	int line;
+};
+
+
+enum class LogLevel { Debug = 0, Info, Warn, Error, Fatal };
+
+class Logger {
+public:
+	static inline void SetLogLevel(LogLevel newLevel) { m_loglevel = newLevel; }
+	
+	static void Log(const std::string& category, const LogReportData& report, const LogLevel level, const std::string& message) {
+#ifndef _DEBUG
+		return;
+#endif
+		if (level < m_loglevel) {
+			return;
+		}
+		if (level < LogLevel::Debug || level > LogLevel::Fatal) {
+			return;
+		}
+
+		auto levelText = m_loglevelTexts[static_cast<int>(level)];
+
+		// 1. [WARN] [parent/category1] "ログメッセージ" (__FILE__ __LINE_ __func__)
+		OutputDebugStringA(fmt::format("[{0}] [{1}] \"{2}\" ({3} {4} {5})\n",
+			levelText ,category, message,
+			report.fileName, report.line, report.funcName).c_str());
+		return;
+	}
+private:
+	static LogLevel m_loglevel;
+	static const std::string m_loglevelTexts[];
+};
+
+}
+
+fne::LogLevel fne::Logger::m_loglevel = fne::LogLevel::Error;
+const std::string fne::Logger::m_loglevelTexts[] = { "DEBUG", " INFO", " WARN", "ERROR", "FATAL" };
+
+#define CURRENT_REPORT_DATA fne::LogReportData{__FILE__,__func__,__LINE__}
+#define LOG_DEBUG(category, log) fne::Logger::Log(category,CURRENT_REPORT_DATA,fne::LogLevel::Debug,log)
+#define LOG_INFO(category, log)  fne::Logger::Log(category,CURRENT_REPORT_DATA,fne::LogLevel::Info,log)
+#define LOG_WARN(category, log)  fne::Logger::Log(category,CURRENT_REPORT_DATA,fne::LogLevel::Warn,log)
+#define LOG_ERROR(category, log) fne::Logger::Log(category,CURRENT_REPORT_DATA,fne::LogLevel::Error,log)
+#define LOG_FATAL(category, log) fne::Logger::Log(category,CURRENT_REPORT_DATA,fne::LogLevel::Fatal,log)
+
--- EngineCore/src/Main.cpp
+++ EngineCore/src/Main.cpp
@@ -8,14 +8,20 @@
 #include "Graphics/DX12TriangleApp.h"
 #include "Graphics/Win32Application.h"
 #include <fmt/core.h>
+#include "Core/Utility/Logger/Logger.h"
 
 _Use_decl_annotations_
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
 {
-	auto text = fmt::format("The answer is {}.\n", 42);
-    OutputDebugStringA(text.c_str());
-
     using namespace fne;
+
+    Logger::SetLogLevel(LogLevel::Debug);
+    LOG_FATAL("main", "logその1");
+    LOG_ERROR("main/category", "logその2");
+    LOG_WARN("main/category2", "logその3");
+    LOG_INFO("main", "logその4");
+    LOG_DEBUG("main", "logその5");
+
     DX12TriangleApp sample(1280, 720, L"D3D12 Hello Triangle");
     return Win32Application::Run(&sample, hInstance, nCmdShow);
 }
