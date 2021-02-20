66bf0709b68a3269f02fd1086e6b570058c0ca8c
ai2playgame <detective9684@gmail.com>
2021-02-20 22:03:43 +0900

    フォルダ構成調整 & fmtライブラリ追加

--- EngineCore/src/Main.cpp
+++ EngineCore/src/Main.cpp
@@ -5,12 +5,16 @@
 //*********************************************************
 
 #include "pch.h"
-#include "Core/DX12TriangleApp.h"
-#include "Core/Win32Application.h"
+#include "Graphics/DX12TriangleApp.h"
+#include "Graphics/Win32Application.h"
+#include <fmt/core.h>
 
 _Use_decl_annotations_
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
 {
+	auto text = fmt::format("The answer is {}.\n", 42);
+    OutputDebugStringA(text.c_str());
+
     using namespace fne;
     DX12TriangleApp sample(1280, 720, L"D3D12 Hello Triangle");
     return Win32Application::Run(&sample, hInstance, nCmdShow);
